b6defa5803f3edb17d68385abab9731187450083
Michał Bugdoł <michalbugdol1024@gmail.com>
2024-02-07 19:50:26 +0100

    Added .clang-format & .clang-tidy files

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,14 +2,15 @@
 #include <QQmlApplicationEngine>
 
 int main(int argc, char** argv) {
-    QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine;
-    QUrl url {u"qrc:/editor/gui/main.qml"_qs};
+  QGuiApplication app(argc, argv);
+  QQmlApplicationEngine engine;
+  const QUrl url{u"qrc:/editor/gui/main.qml"_qs};
 
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-        QCoreApplication::instance(), [](){QCoreApplication::instance()->exit(1);},
-        Qt::QueuedConnection);
-    engine.load(url);
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreationFailed,
+      QCoreApplication::instance(),
+      []() { QCoreApplication::exit(1); }, Qt::QueuedConnection);
+  engine.load(url);
 
-    return app.exec();
+  return app.exec();
 }
